3d79290b95b50e6a717e42406ac6d290fd90b85b
Joe C <52496811+Jcorb08@users.noreply.github.com>
2021-01-15 15:36:08 +0000

    Add files via upload

---
+++ Lab3-Part C-PHP/InsertSQLSystemUser.sql
@@ -0,0 +1,2 @@
+INSERT INTO SystemUser (Username,Password,Forename,Surname,Email)
+VALUES ('heyname','password', 'hey', 'name', 'hey@name.com');
---
+++ Lab3-Part C-PHP/pizza.sql
@@ -0,0 +1,9 @@
+Create Table Customer(
+customer_ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+customerName Char(30),
+customerPass Char(30),
+customerEmailAddress Char(255),
+dateOfBirth Date,
+Address Char(255)
+
+);
